ad5e49b9f7bc0c0eac9f4645d98fd006fed4b988
Jose SR <josesanchez09190@gmail.com>
2024-05-16 19:31:51 -0600

    added basic user entries in main

--- src/jsr/project/currency_converter/main/Main.java
+++ src/jsr/project/currency_converter/main/Main.java
@@ -1,7 +1,69 @@
 package jsr.project.currency_converter.main;
 
+import jsr.project.currency_converter.utils.config.Configuration;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String apiKey = Configuration.API_KEY;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Ingresa tu moneda actual (por ejemplo, USD)");
+            String currentCurrencyCode = scanner.nextLine().toUpperCase();
+
+            if (currencyCodeIsValid(currentCurrencyCode)) {
+                System.out.println("Ingresa la moneda deseada (por ejemplo, MXN):");
+                String desiredCurrencyConvert = scanner.nextLine().toUpperCase();
+
+                if (currencyCodeIsValid(desiredCurrencyConvert)) {
+
+                    double amountOfMoney = 0;
+
+                    do {
+                        System.out.println("Ingresa la cantidad de " + currentCurrencyCode + " a convertir:");
+
+                        // Verificar si el siguiente token es un double
+                        if (scanner.hasNextDouble()) {
+                            amountOfMoney = scanner.nextDouble();
+
+                            // Consumir el salto de línea pendiente
+                            scanner.nextLine();
+
+                            break; // Salir del bucle si se ingresa un double válido
+                        } else {
+                            System.out.println("Entrada inválida. Por favor, ingresa una cantidad válida.");
+                            scanner.nextLine(); // Limpiar la entrada del scanner
+                        }
+                    } while (true); // Continuar hasta que se ingrese un double válido
+
+                    String uri = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/" + currentCurrencyCode;
+
+                    System.out.println();
+                    System.out.println("Ingresa una S para continuar ó una N para terminar");
+                    String exit = scanner.nextLine();
+                    if (exit.equalsIgnoreCase("N")) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println("Finalizo la ejecución");
+    }
+
+    private static boolean currencyCodeIsValid(String currencyCode) {
+        try {
+            // Intenta convertir el código de moneda a un número
+            Integer.parseInt(currencyCode);
+            // Si no se lanza una excepción, significa que es un número, por lo que el código de moneda no es válido
+            return false;
+        } catch (NumberFormatException e) {
+            // Si se lanza una excepción, significa que no es un número, por lo que el código de moneda es válido
+            return true;
+        }
     }
 }
---
+++ src/jsr/project/currency_converter/models/ExchangeRate.java
@@ -0,0 +1,16 @@
+package jsr.project.currency_converter.models;
+
+import java.util.Map;
+
+// Tipo de cambio
+public record ExchangeRate(
+        String result,
+        String documentation,
+        String terms_of_use,
+        long time_last_update_unix,
+        String time_last_update_utc,
+        long time_next_update_unix,
+        String time_next_update_utc,
+        String base_code,
+        Map<String, Double> conversion_rates
+) { }
