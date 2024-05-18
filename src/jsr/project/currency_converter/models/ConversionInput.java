fe9728061265b211c3513afcc48b36aee89b9a6a
Jose SR <josesanchez09190@gmail.com>
2024-05-18 01:12:20 -0600

    updated conversion input to a record class

--- src/jsr/project/currency_converter/models/ConversionInput.java
+++ src/jsr/project/currency_converter/models/ConversionInput.java
@@ -1,19 +1,4 @@
 package jsr.project.currency_converter.models;
 
-public class ConversionInput {
-    private String desiredCurrency;
-    private double amount;
-
-    public ConversionInput(String desiredCurrency, double amount) {
-        this.desiredCurrency = desiredCurrency;
-        this.amount = amount;
-    }
-
-    public String getDesiredCurrency() {
-        return desiredCurrency;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
+public record ConversionInput(String desiredCurrency, double amount) {
 }
